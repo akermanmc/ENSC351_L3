e46a56fc4ec1632f6496b1fca001aaa2217c8a8a
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-10-14 16:01:14 -0700

    implemented simple word count

--- word_count.cpp
+++ word_count.cpp
@@ -2,18 +2,42 @@
 
 
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 
 using namespace std;
 
-int main(int argc, char** argv){
+int main(){
 	// read in file
+	ifstream inFile;
+	inFile.open("words.txt");
+	if(!inFile.is_open()) 
+		return 1;
+
+	string word;
+	int count = 0;
+	while (inFile >> word){
+		count ++;
+	}
+
+	inFile.close();
+
 
 	// map
 
 	// reduce
 
 	// output
+	ofstream outFile;
+	outFile.open("word_count.txt");
+
+	outFile << count;
+
+	outFile.close();
+
+	return 0;
+
 }
