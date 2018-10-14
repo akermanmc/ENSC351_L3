b5d3f3ad0ac026d4cbbfbc2b724e3fd5c6bad1b1
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-10-14 15:32:25 -0700

    Add files via upload

---
+++ word_count.cpp
@@ -0,0 +1,19 @@
+//word count with mapreduce
+
+
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	// read in file
+
+	// map
+
+	// reduce
+
+	// output
+}
