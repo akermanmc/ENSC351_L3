45f69c280c579fcf0d7e25409587c8455a2ebb84
Aayush Malhotra <aayushm72@gmail.com>
2018-10-15 22:16:16 -0700

    Multi-threaded completed

--- word_countMR.cpp
+++ word_countMR.cpp
@@ -4,17 +4,28 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <thread>
 #include <pthread.h>
 #include <unistd.h>
 #include <string.h>
 #include <utility>
 #include <vector>	
 #include <algorithm>
-
+#define StringIntPair pair<string, int>
 using namespace std;
 
-int count = 0;
 ifstream inFile;
+int numThreads;
+vector<string> word_vector; // holds input item: vector of words parsed from input file
+vector<StringIntPair> pair_vector;	// holds initial mapped pairs
+vector<vector<StringIntPair>> dup_vector;	// vector of vector!! wohoo! holds duplicate pairs to be reduced as distinct vectors
+vector<StringIntPair> reduced_vector;	// holds the final reduced pairs for word_count output
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+bool compFuncDescending(const StringIntPair a, const StringIntPair b)
+{
+    return (a.second > b.second);
+}
 
 // input function
 vector<string> inputReadFunc(vector<string> word_vector){
@@ -26,36 +37,57 @@ vector<string> inputReadFunc(vector<string> word_vector){
 }
 
 // map function
-pair<string, int> mapFunc(string word){
+pair<string, int> mapFunc(const string &word){
 	int value = 1;
-	pair<string, int> key_val_pair;
+    StringIntPair key_val_pair;
 	key_val_pair.first = word;
 	key_val_pair.second = value;
 	return key_val_pair;
 }
 
 //reduce function
-pair<string,int> reduceFunc(vector<pair<string,int> > dup_vector){
-	pair<string, int> combined_pair;
+pair<string,int> reduceFunc(const vector<StringIntPair> &dup_vector){
+    StringIntPair combined_pair;
 	combined_pair.first = dup_vector[0].first;
 	combined_pair.second = dup_vector.size();
 	return combined_pair;
 }
 
 //output function
-void outputFunc(vector<pair<string, int> > reduced_vector){
+void outputFunc(const vector<StringIntPair> reduced_vector){
 	for (int i = 0; i<reduced_vector.size(); i++){
 		cout << reduced_vector[i].first << "," << reduced_vector[i].second << endl;
 	}
 }
 
-int main(){
-	vector<string> word_vector; // holds input item: vector of words parsed from input file
-	vector<pair<string, int> > pair_vector;	// holds initial mapped pairs
-	vector<pair<string, int> > dup_vector;	// holds duplicate pairs to be reduced
-	vector<pair<string, int> > used_vector;	// keeps track of which pairs have been tested for duplicity
-	vector<pair<string, int> > reduced_vector;	// holds the final reduced pairs for word_count output
+//thread proc for Mapping
+void* mapProc(void* arg){
+    int pos = *(int*)arg;
+    int size = word_vector.size();
+    while(pos < size)
+    {
+        StringIntPair temp = mapFunc(word_vector[pos]);
+        pthread_mutex_lock(&lock);
+        pair_vector.push_back(temp);
+        pthread_mutex_unlock(&lock);
+        pos += numThreads;
+    }
+}
 
+//thread proc for Reduce
+void* reduceProc(void* arg){
+    int pos = *(int*)arg;
+    int size = dup_vector.size();
+    while(pos < size){
+        StringIntPair temp = reduceFunc(dup_vector[pos]);
+        pthread_mutex_lock(&lock);
+        reduced_vector.push_back(temp);
+        pthread_mutex_unlock(&lock);
+        pos += numThreads;
+    }
+}
+
+int main(){
 	// STEP 1: read in file
 	inFile.open("words.txt");
 	if(!inFile.is_open()){
@@ -65,36 +97,53 @@ int main(){
 	word_vector = inputReadFunc(word_vector);
 
 	inFile.close();
+    //get number of threads
+	numThreads = thread::hardware_concurrency();
+    vector<pthread_t> threadVector(numThreads);
+    vector<int> threadPosVector(numThreads);
 
-
-	// STEP 2: map
-	for (int i = 0; i<word_vector.size(); i++){
-		pair_vector.push_back(mapFunc(word_vector[i]));
+    // STEP 2: map
+	for (int i = 0; i<numThreads; i++){
+	    threadPosVector[i] = i;
+		pthread_create(&threadVector[i], NULL, mapProc, &threadPosVector[i]);
 	}
 
-	// STEP 3: reduce: send vectors of duplicates to reduce function
-	for (int i= 0; i<pair_vector.size(); i++){
-		if (find(used_vector.begin(), used_vector.end(), pair_vector[i]) != used_vector.end()){
-			continue;
-		}
-		dup_vector.push_back(pair_vector[i]);
-		used_vector.push_back(pair_vector[i]);
-		for (int j = i+1; j<pair_vector.size(); j++){
-			if (find(dup_vector.begin(), dup_vector.end(), pair_vector[j]) != dup_vector.end()){
-				dup_vector.push_back(pair_vector[j]);
-			}
-		}
-		/*for (int i=0; i<dup_vector.size();i++)
-			cout << dup_vector[i].first << dup_vector[i].second << endl;
-		cout << endl;*/
-		reduced_vector.push_back(reduceFunc(dup_vector));
-		dup_vector.clear();
+	for (int i = 0; i<numThreads; i++){
+	    pthread_join(threadVector[i],NULL);
 	}
 
+	//form dup_vector structure with separate vectors for all unique words
+	for (int i = 0; i < pair_vector.size(); i++)
+    {
+	    int j =0;
+	    for (j = 0; j < dup_vector.size(); j++)
+	    {
+            if (pair_vector[i].first == dup_vector[j][0].first)
+            {
+                dup_vector[j].push_back(pair_vector[i]);
+                break;
+            }
+        }
+        if(j >= dup_vector.size())
+        {
+            vector<StringIntPair> temp;
+            temp.push_back(pair_vector[i]);
+            dup_vector.push_back(temp);
+        }
+    }
+    //STEP3: reduce
+    for (int i = 0; i<numThreads; i++){
+        threadPosVector[i] = i;
+        pthread_create(&threadVector[i], NULL, reduceProc, &threadPosVector[i]);
+    }
 
-	// STEP 4: output
-	outputFunc(reduced_vector);
+    for (int i = 0; i<numThreads; i++){
+        pthread_join(threadVector[i],NULL);
+    }
 
-	return 0;
+    // STEP 3.5: sort the reduced vector in descending
+    std::sort(reduced_vector.begin(), reduced_vector.end(), compFuncDescending);
 
+    // STEP 4: output
+    outputFunc(reduced_vector);
 }
